1107ba91adedfcaa32a908a537d391f01b357581
elegantmoonstudio <elegantmoonstudio@gmail.com>
2023-07-12 15:16:56 +0700

    Fix rotated img func

--- utils/functions.py
+++ utils/functions.py
@@ -33,7 +33,6 @@ def align_face(raw_face, left_eye, right_eye):
         angle = 90 - angle
 
     # Rotate the face image by 30 degrees
-    angle = 30
     rows, cols = raw_face.shape[:2]
     rotation_matrix = cv2.getRotationMatrix2D((cols/2, rows/2), angle * direction, 1)
     rotated_image = cv2.warpAffine(raw_face, rotation_matrix, (cols, rows))
